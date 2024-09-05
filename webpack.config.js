19f40de3c0ba7cf330a64742681fee67e7845bf7
raduserb <radu.serb06@gmail.com>
2024-09-05 16:09:38 +0300

    hw done

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', // Adjust this to your actual entry point
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
+
